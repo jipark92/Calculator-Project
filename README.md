f06b90cb1d797ee5b6ec66ce71f1abb0e42b66bf
ji <jiyongprk@gmail.com>
2022-01-18 14:19:36 -0500

    buttons working

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 Hello this is my calculator project.
 
-Day 1 12:00AM (1/18/2022): laying out some buttons and display visual and pre-assigning class and id selectors to button. 
+Day 1 12:00AM (1/18/2022): Laying out some buttons and display visual and pre-assigning class and id selectors to button. 
 
-Day 2 9:51AM (1/18/2022) : laid out all the button for now and added a mouseover color change effect for each button.
+Day 2 9:51AM (1/18/2022) : Laid out all the button for now and added a mouseover color change effect for each button.
 
-Day 2 1:45PM (1/18/2022) : assign quereySelector to all the buttons
+    1:45PM  : Assign quereySelector to all the buttons
+
+    2:15PM : numbers and operators show up on display when buttons are pressed.
+    
+        -note: I have to somehow assign numbers and operators to a variable, so i can later add,subtract,multiply, and divide with two numbers.
+        (difficult javascript parts now)
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const equalSign = document.querySelector('#equal-sign');
 const plusSign = document.querySelector('#plus-sign');
 const minusSign = document.querySelector('#minus-sign');
 const multiplySign = document.querySelector('#multiply-sign');
-const divide = document.querySelector('#divide-sign');
+const divideSign = document.querySelector('#divide-sign');
 
 const btnOne = document.querySelector('#btn-one');
 const btnTwo = document.querySelector('#btn-two');
@@ -16,6 +16,84 @@ const btnSeven = document.querySelector('#btn-seven');
 const btnEight = document.querySelector('#btn-eight');
 const btnNine = document.querySelector('#btn-nine');
 const btnZero = document.querySelector('#btn-zero');
-const btnClear = document.querySelector('#btn-clean');
+const btnClear = document.querySelector('#btn-clear');
+
+ 
 
 
+
+
+function operate (){
+    plusSign.addEventListener('click', ()=> {
+        display.textContent = ' + ' ;
+    }),
+
+    minusSign.addEventListener('click', ()=> {
+        display.textContent =  '-' ;
+    }),
+
+    multiplySign.addEventListener('click', ()=> {
+        display.textContent = "x" ;
+    }),
+
+    divideSign.addEventListener('click', ()=> {
+        display.textContent = "/" ;
+    }),
+
+    equalSign.addEventListener('click', ()=> {
+        display.textContent = '=';
+    })
+
+}
+
+operate();
+
+function clear(){
+    btnClear.addEventListener('click', ()=> {
+        display.textContent = '';
+    })
+}
+clear();
+
+function numberPads(){
+    btnOne.addEventListener('click', ()=> {
+        display.textContent = "1";
+    }),
+
+    btnTwo.addEventListener('click', ()=> {
+        display.textContent = "2";
+    }),
+
+    btnThree.addEventListener('click', ()=> {
+        display.textContent = "3";
+    }),
+
+    btnFour.addEventListener('click', ()=> {
+        display.textContent = "4";
+    }),
+
+    btnFive.addEventListener('click', ()=> {
+        display.textContent = "5";
+    }),
+
+    btnSix.addEventListener('click', ()=> {
+        display.textContent = "6";
+    }),
+    btnSeven.addEventListener('click', ()=> {
+        display.textContent = "7";
+    }),
+    btnEight.addEventListener('click', ()=> {
+        display.textContent = "8";
+    }),
+
+    btnNine.addEventListener('click', ()=> {
+        display.textContent = "9";
+    }),
+
+    btnZero.addEventListener('click', ()=> {
+        display.textContent = "0";
+    })
+
+
+}
+numberPads();
