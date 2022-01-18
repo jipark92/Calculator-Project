c6e06369ac3ab3f76827a6059c765599ae98cea2
ji <jiyongprk@gmail.com>
2022-01-18 13:47:18 -0500

    querySelector to button

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Hello this is my calculator project.
 
-day 1 12:00AM (1/18/2022): laying out some buttons and display visual and pre-assigning class and id selectors to button. 
+Day 1 12:00AM (1/18/2022): laying out some buttons and display visual and pre-assigning class and id selectors to button. 
 
-day 2 9:51AM (1/18/2022) : laid out all the button for now and added a mouseover color change effect for each button.
+Day 2 9:51AM (1/18/2022) : laid out all the button for now and added a mouseover color change effect for each button.
+
+Day 2 1:45PM (1/18/2022) : assign quereySelector to all the buttons
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-//nothing for now
+const display = document.querySelector('#display-number');
+
+const equalSign = document.querySelector('#equal-sign');
+const plusSign = document.querySelector('#plus-sign');
+const minusSign = document.querySelector('#minus-sign');
+const multiplySign = document.querySelector('#multiply-sign');
+const divide = document.querySelector('#divide-sign');
+
+const btnOne = document.querySelector('#btn-one');
+const btnTwo = document.querySelector('#btn-two');
+const btnThree = document.querySelector('#btn-three');
+const btnFour = document.querySelector('#btn-four');
+const btnFive = document.querySelector('#btn-five');
+const btnSix = document.querySelector('#btn-six');
+const btnSeven = document.querySelector('#btn-seven');
+const btnEight = document.querySelector('#btn-eight');
+const btnNine = document.querySelector('#btn-nine');
+const btnZero = document.querySelector('#btn-zero');
+const btnClear = document.querySelector('#btn-clean');
+
+
