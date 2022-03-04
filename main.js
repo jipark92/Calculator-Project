9170e22957efa3113a5ba150dac09417ed7393c3
ji <jiyongprk@gmail.com>
2022-03-04 16:29:10 -0500

    start on javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+const prevDisplay = document.querySelector('.prev-display');
+const currDisplay = document.querySelector('.curr-display');
+const operands = document.querySelectorAll('.operands');
+const operators = document.querySelector('.operators');
+const clearBtn = document.querySelector('.clear-sign');
+const deleteBtn = document.querySelector('.delete-sign');
+const equalBtn = document.querySelector('.equal-sign');
+
+
+console.log(operands);
