72e2baa0f0204d5d477f5ebc2c28cb188b48209a
ji <jiyongprk@gmail.com>
2022-03-05 08:13:53 -0500

    most javascript working

--- main.js
+++ main.js
@@ -5,81 +5,91 @@ const operators = document.querySelectorAll('.operators');
 const clearBtn = document.querySelector('.clear-sign');
 const deleteBtn = document.querySelector('.delete-sign');
 const equalBtn = document.querySelector('.equal-sign');
-//variables 
+
 let operandsA = "";
 let operatorSign = "";
 let operandsB = "";
 let totaResult = "";
-//make operands work
-function submitOperands() {
-    operands.forEach((operand) => {
-        operand.addEventListener('click', ()=>{
-            if (!operatorSign){
-                operandsA += operand.value;
-                prevDisplay.textContent = operandsA;
-                // console.log(operandsA, "op1");  
-            } 
-            else if (operatorSign === "-"||operatorSign === "+"||operatorSign === "*"||operatorSign === "/"){
-                operandsB += operand.value;
-                currDisplay.textContent = operandsB;
-                // console.log(operandsB, "op2");
+
+function allButtons(){
+    function submitOperands() {
+        operands.forEach((operand) => {
+            operand.addEventListener('click', ()=>{
+                if (!operatorSign){
+                    operandsA += operand.value;
+                    prevDisplay.textContent = operandsA;
+                    // console.log(operandsA, "op1");  
+                } 
+                else if (operatorSign === "-"||operatorSign === "+"||operatorSign === "*"||operatorSign === "/"){
+                    operandsB += operand.value;
+                    currDisplay.textContent = operandsB;
+                    // console.log(operandsB, "op2");
+                }
+            })
+        })
+    };
+    submitOperands();
+    function submitOperator() {
+        operators.forEach((operator)=> {
+            operator.addEventListener('click', ()=>{
+                operatorSign = operator.value;
+                prevDisplay.textContent += operatorSign;
+            })
+        })
+    };
+    submitOperator();
+    function eqaulButton(){
+        equalBtn.addEventListener('click',()=>{
+            if (operatorSign === '+'){
+                prevDisplay.textContent = parseInt(operandsA) + parseInt(operandsB) ;
+                currDisplay.textContent = ""
+                operandsA = prevDisplay.textContent;
+                operandsB = "";
+                return;
+            } else if (operatorSign === '-'){
+                prevDisplay.textContent = parseInt(operandsA) - parseInt(operandsB);
+                currDisplay.textContent = ""
+                operandsA = prevDisplay.textContent;
+                operandsB = "";
+                return;
+            } else if (operatorSign === '*'){
+                prevDisplay.textContent = parseInt(operandsA) * parseInt(operandsB);
+                currDisplay.textContent = ""
+                operandsA = prevDisplay.textContent;
+                operandsB = "";
+                return;
+            } else if (operatorSign === '/'){
+                prevDisplay.textContent = parseInt(operandsA) / parseInt(operandsB);
+                currDisplay.textContent = ""
+                operandsA = prevDisplay.textContent;
+                operandsB = "";
+                return;
             }
         })
-    })
-};
-submitOperands();
-//make operators work
-function submitOperator() {
-    operators.forEach((operator)=> {
-        operator.addEventListener('click', ()=>{
-            operatorSign = operator.value;
-            prevDisplay.textContent += operatorSign;
+    };
+    eqaulButton();
+    function clearButton(){
+        clearBtn.addEventListener('click',()=>{
+            prevDisplay.textContent = "";
+            currDisplay.textContent = "";
+            operandsA = "";
+            operandsB = "";
+            operatorSign = "";
+            totaResult = "";
         })
-    })
-};
-submitOperator();
-//make equal work
-function totalResults(){
-    equalBtn.addEventListener('click',()=>{
-        if (operatorSign === '+'){
-            prevDisplay.textContent = parseInt(operandsA) + parseInt(operandsB) ;
-            currDisplay.textContent = ""
-        } else if (operatorSign === '-'){
-            prevDisplay.textContent = parseInt(operandsA) - parseInt(operandsB);
-            currDisplay.textContent = ""
-        } else if (operatorSign === '*'){
-            prevDisplay.textContent = parseInt(operandsA) * parseInt(operandsB);
-            currDisplay.textContent = ""
-        } else if (operatorSign === '/'){
-
-            prevDisplay.textContent = parseInt(operandsA) / parseInt(operandsB);
-            currDisplay.textContent = ""
-        }
-    })
-};
-totalResults()
-//make clear work
-function clearButton(){
-    clearBtn.addEventListener('click',()=>{
-        prevDisplay.textContent = "";
-        currDisplay.textContent = "";
-        operandsA = "";
-        operandsB = "";
-        operatorSign = "";
-        totaResult = "";
-    })
-};
-clearButton();
-//make delete work
-function deleteButton() {
-    deleteBtn.addEventListener('click',()=>{
-        prevDisplay.textContent = operandsA.slice(0,-1)
-        prevDisplay.textContent.slice(0,-1);
-        console.log(operandsA, 'click')
-        console.log(prevDisplay.textContent, 'click')
-    })
+    };
+    clearButton();
+    function deleteButton() {
+        deleteBtn.addEventListener('click',()=>{
+            prevDisplay.textContent = operandsA.slice(0,-1)
+            prevDisplay.textContent.slice(0,-1);
+            console.log(operandsA, 'click')
+            console.log(prevDisplay.textContent, 'click')
+        })
+    }
+    deleteButton();
 }
-deleteButton();
+allButtons();
 
 
 
