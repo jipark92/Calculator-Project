719188eb8b8655ea36106b67d3839941322143fd
ji <jiyongprk@gmail.com>
2022-03-05 00:24:33 -0500

    work on javascript

--- main.js
+++ main.js
@@ -1,31 +1,106 @@
 const prevDisplay = document.querySelector('.prev-display');
 const currDisplay = document.querySelector('.curr-display');
 const operands = document.querySelectorAll('.operands');
-const operators = document.querySelector('.operators');
+const operators = document.querySelectorAll('.operators');
 const clearBtn = document.querySelector('.clear-sign');
 const deleteBtn = document.querySelector('.delete-sign');
 const equalBtn = document.querySelector('.equal-sign');
+const operatorDisplay = document.querySelector('.operator-display');
 
 
 //variables 
-let operandsA = 0;
-let operandsB = 0;
-let operatorSign = null;
+let operandsA = "";
+let operatorSign = "";
+let operandsB = "";
+// let operatoSign = submitOperator(); 
 
-let currNumber = currDisplay;
 
 
 
 
 //make operands work
-
-function submitOperands() {
+function submitOperandsA() {
     operands.forEach((operand) => {
         operand.addEventListener('click', ()=>{
-            console.log(operand.textContent);
-            currDisplay.textContent += operand.textContent;
-            console.log(currNumber);
+            if (!prevDisplay.textContent.includes('+', '-', '/', '*')){
+                operandsA += operand.value;
+                prevDisplay.textContent = operandsA
+                console.log(operandsA, "op1")
+            }
+
+
+
+            
+            else if (prevDisplay.textContent.includes('+', '-', '/', '*')){
+                operandsB += operand.value;
+                prevDisplay.textContent += operandsB
+                console.log(operandsB, "op2")
+            }
         })
     })
-}
-submitOperands();
+};
+submitOperandsA();
+
+// function submitOperandsB() {
+//     operands.forEach((operand) => {
+//         operand.addEventListener('click', ()=>{
+//             currDisplay.textContent += operand.textContent;
+//             // console.log(currDisplay.textContent)
+//             operandsB = currDisplay.textContent;
+//             console.log(operandsB)
+//         })
+//     })
+// };
+
+
+
+
+
+// function totalResult() {
+//     equalBtn.addEventListener('click', ()=>{
+//         if (currDisplay.textContent.includes('+')) {
+//             return operandsA + operandsB;
+//         } else if (currDisplay.textContent.includes('-')){
+//             return operandsA - operandsB;
+//         } else if (currDisplay.textContent.includes('*')){
+//             return operandsA * operandsB;
+//         } else if (currDisplay.textContent.includes('/')){
+//             return operandsA / operandsB;
+//         }
+//         console.log(operatoSign)
+//     })
+// }
+// totalResult();
+
+
+
+// if (currDisplay.textContent.includes('+', '-', '/', '*')){
+                
+// }
+
+function submitOperator() {
+    operators.forEach((operator)=> {
+        operator.addEventListener('click', ()=>{
+            operatorSign = operator.value;
+            prevDisplay.textContent += operator.value;
+            console.log(operatorSign)
+            // console.log(operatorSign);
+            // if (currDisplay.textContent.includes('+')){
+            //     console.log(currDisplay.textContent)
+            //     return  "+";
+               
+            // } else if (currDisplay.textContent.includes('-')){
+            //     return  "-";
+                
+            // } else if (currDisplay.textContent.includes('*')){
+            //     return  "*";
+                
+            // } else if (currDisplay.textContent.includes('/')){
+            //     return  "/";
+            // }
+        })
+        
+    })
+
+};
+submitOperator();
