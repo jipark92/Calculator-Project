bad24e3dfd4222110ae4d6cb17162ff3869d6723
ji <jiyongprk@gmail.com>
2022-03-05 02:22:11 -0500

    equal sign working

--- main.js
+++ main.js
@@ -5,46 +5,61 @@ const operators = document.querySelectorAll('.operators');
 const clearBtn = document.querySelector('.clear-sign');
 const deleteBtn = document.querySelector('.delete-sign');
 const equalBtn = document.querySelector('.equal-sign');
-const operatorDisplay = document.querySelector('.operator-display');
 //variables 
 let operandsA = "";
 let operatorSign = "";
 let operandsB = "";
 //make operands work
-function submitOperandsA() {
+function submitOperands() {
+    //make operands work
     operands.forEach((operand) => {
         operand.addEventListener('click', ()=>{
             if (!operatorSign){
                 operandsA += operand.value;
-                prevDisplay.textContent = operandsA
+                prevDisplay.textContent = operandsA;
                 console.log(operandsA, "op1")
+                
             } 
             else if (operatorSign === "-"||operatorSign === "+"||operatorSign === "*"||operatorSign === "/"){
                 operandsB += operand.value;
-                currDisplay.textContent = operandsB
-                console.log(operandsB, "op2")
+                currDisplay.textContent = operandsB;
+                console.log(operandsB, "op2");
             }
         })
     })
 };
-submitOperandsA();
+submitOperands();
 //make operators work
 function submitOperator() {
     operators.forEach((operator)=> {
         operator.addEventListener('click', ()=>{
-            operatorSign = operator.value
+            operatorSign = operator.value;
             prevDisplay.textContent += operatorSign;
-            console.log(operatorSign)
+            console.log(operatorSign);
         })
     })
 };
 submitOperator();
 //make equal button work
 function totalResults(){
-    
+    equalBtn.addEventListener('click',()=>{
+        if (operatorSign === '+'){
+            console.log('+ clicked');
+            prevDisplay.textContent = parseInt(operandsA) + parseInt(operandsB) ;
+        } else if (operatorSign === '-'){
+            console.log('- clicked');
+            prevDisplay.textContent = operandsA - operandsB;
+        } else if (operatorSign === '*'){
+            console.log('* clicked');
+            prevDisplay.textContent = operandsA * operandsB;
+        } else if (operatorSign === '/'){
+            console.log('/ clicked');
+            prevDisplay.textContent = operandsA / operandsB;
+        }
+        
+    })
 }
-
-
+totalResults()
 
 
 
